f75d0f5ad551738174b4c266c7036d977d11553b
Marc Bäckman <93243973+mabackma@users.noreply.github.com>
2024-05-21 01:39:25 +0300

    heatmaps with hourly values

--- data_analyzer.py
+++ data_analyzer.py
@@ -3,9 +3,6 @@ import polars as pl
 import pandas as pd
 import streamlit as st
 from sklearn.preprocessing import MinMaxScaler
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.graph_objects as go
 import plotly.express as px
 
 def callback_query():
