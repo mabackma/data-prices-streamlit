165420ead60fd8eba33ece39e4cec19306f26c5c
Marc Bäckman <93243973+mabackma@users.noreply.github.com>
2024-05-21 02:12:18 +0300

    heatmaps drawn side by side

--- data_analyzer.py
+++ data_analyzer.py
@@ -168,9 +168,15 @@ class DataAnalyzer:
 
                     st.write(f'<h3>Location: {location}</h3>', unsafe_allow_html=True)
                     st.write(f'<h4>Time range: {start} - {end}</h4>', unsafe_allow_html=True)
-                    for line in lines:
+
+
+                    # Draw heatmaps in columns
+                    columns = st.columns(len(lines))
+                    for i, line in enumerate(lines, start=0):
                         heatmap_data = location_df.pivot_table(index='day', columns='hour', values=line, aggfunc='mean')
-                        draw_heatmap(heatmap_data, line)
+                        with columns[i]:
+                            draw_heatmap(heatmap_data, line)
+
                 else:
                     st.write('Choose columns to draw heatmap')
             else:
