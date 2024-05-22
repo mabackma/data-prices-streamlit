f7019a56975a17b2eceb0a64357ac8223230c9a9
Marc Bäckman <93243973+mabackma@users.noreply.github.com>
2024-05-22 11:21:32 +0300

    changed max upload size to 800MB

--- data_analyzer.py
+++ data_analyzer.py
@@ -150,7 +150,7 @@ class DataAnalyzer:
                     # Draw the line chart
                     st.write(f'<h2>{location_names[location]}</h2>', unsafe_allow_html=True)
                     st.write(f'<h4>meter_id: {location}</h4>', unsafe_allow_html=True)
-                    st.write(f'<h4>Time range: {start.date()} - {end.date()}</h4>', unsafe_allow_html=True)
+                    st.write(f'<h4>Time range: {start} - {end}</h4>', unsafe_allow_html=True)
                     st.line_chart(hourly_df[lines])
                 else:
                     st.write('Choose columns to draw line chart')
@@ -165,7 +165,7 @@ class DataAnalyzer:
         if st.session_state.heatmap_button_clicked:
             if len(location_df) > 0:
                 if len(sensors) > 0:
-                    #location_df[sensors] = location_df[sensors].fillna(0)
+                    location_df[sensors] = location_df[sensors].fillna(0)
 
                     # Prepare data for heatmap
                     location_df['hour'] = location_df.index.hour
@@ -173,7 +173,7 @@ class DataAnalyzer:
 
                     st.write(f'<h2>{location_names[location]}</h2>', unsafe_allow_html=True)
                     st.write(f'<h4>meter_id: {location}</h4>', unsafe_allow_html=True)
-                    st.write(f'<h4>Time range: {start.date()} - {end.date()}</h4>', unsafe_allow_html=True)
+                    st.write(f'<h4>Time range: {start} - {end}</h4>', unsafe_allow_html=True)
 
                     # Draw heatmaps in columns
                     columns = st.columns(len(sensors))
